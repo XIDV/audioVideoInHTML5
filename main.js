726ef4d8b5c72a49a5c91809be6e9cc60d359f70
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-10-15 10:51:10 +0200

    Beispiel 1b mit CustomPlayerControls begonnen.

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', dcl => {
     console.log('Document is ready ...');
+
+    const bspl1bAudio = document.querySelector('#bspl1bAudio');
+
+    document.querySelector('#bspl1bPlayPause').addEventListener('click', e => {
+        if(bspl1bAudio.paused) {
+            bspl1bAudio.play();
+            e.target.textContent = 'Pause';
+        } else {
+            bspl1bAudio.pause();
+            e.target.textContent = 'Play';
+        }
+    });
+
+
+    bspl1bAudio.addEventListener('play', e => {
+        const showTimeDuration = document.querySelector('#showTimeDuration');
+        setInterval(() => {
+            showTimeDuration.value = `${Math.round(e.target.currentTime)} / ${Math.round(e.target.duration)}`;
+
+        }, 1000);
+    });
+
 });

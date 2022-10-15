9cbf0b5da65cd42b4bc97750c00c10247dd242a4
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-10-15 16:33:39 +0200

    bspl1B Volume-Slider erweitert, Duration-Slider erstellt, rudimentäre Funktionalität hergestellt.

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 document.addEventListener('DOMContentLoaded', dcl => {
     console.log('Document is ready ...');
 
+    
+    // Custom-Audio-Controls Konfiguration ####################################
+    // Controls
     const bspl1bAudio = document.querySelector('#bspl1bAudio');
+    const showTimeDuration = document.querySelector('#showTimeDuration');
+    const setVolumeSlider = document.querySelector('#setVolume');
+    const setDurationSlider = document.querySelector('#durationSlider');
+    // Hilfsvariablen
+    let isOverDurSlider = false;
 
+    // EL f. Anzeige von currentTime und duration, sowie setDurationSlider
+    bspl1bAudio.addEventListener('timeupdate', e => {
+        const mediaCurrentTime = Math.round(e.target.currentTime);
+        showTimeDuration.value = `${mediaCurrentTime} / ${Math.round(e.target.duration)}`;
+
+        if(!isOverDurSlider) {
+            setDurationSlider.value = mediaCurrentTime;
+        }
+
+    });
+
+    // EL f. Initiale Anzeige von showTimeDuration
+    bspl1bAudio.addEventListener('canplaythrough', e => {
+        const mediaDuration = Math.round(e.target.duration);
+        showTimeDuration.value = `0 / ${mediaDuration}`;
+        setDurationSlider.setAttribute('max', `${mediaDuration}`);
+    });
+    
+    // EL f. Start/Pause-Button
     document.querySelector('#bspl1bPlayPause').addEventListener('click', e => {
         if(bspl1bAudio.paused) {
             bspl1bAudio.play();
@@ -13,13 +40,22 @@ document.addEventListener('DOMContentLoaded', dcl => {
         }
     });
 
+    // EL f. setVolumeSlider
+    setVolumeSlider.addEventListener('input', e => {
+        bspl1bAudio.volume = parseFloat(e.target.value) / 100;
+    });
 
-    bspl1bAudio.addEventListener('play', e => {
-        const showTimeDuration = document.querySelector('#showTimeDuration');
-        setInterval(() => {
-            showTimeDuration.value = `${Math.round(e.target.currentTime)} / ${Math.round(e.target.duration)}`;
+    // EL f. Steuerung des Wertes von isOverSlider-Hilfsvariablen
+    setDurationSlider.addEventListener('mouseover', e => {
+        isOverDurSlider = true;
+    });
+    setDurationSlider.addEventListener('mouseleave', e => {
+        isOverDurSlider = false;
+    });
 
-        }, 1000);
+    // EL f. Wertzuweisung von currentTime von bspl1bAudio i. A. von setDurationSlider value
+    setDurationSlider.addEventListener('mouseup', e => {
+        bspl1bAudio.currentTime = parseFloat(e.target.value);
     });
 
 });
